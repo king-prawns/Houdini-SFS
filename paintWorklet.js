ef8a2542b5c209b4977e6529f36d6e69812525f1
Giorgio Gamberoni <giorgio.gamberoni@musement.com>
2019-12-06 13:30:54 +0100

    update

--- paintWorklet.js
+++ paintWorklet.js
@@ -1,8 +1,22 @@
 registerPaint('mybackground', class {
   static get inputProperties() {
-      return ['--my-background'];
+      return ['--multiplier', '--color', '--pad', '--slant'];
   }
   paint(ctx, size, properties) {
-    eval(properties.get('--my-background').toString())(ctx, size, properties);
+    let multiplier = +properties.get('--multiplier');
+    let red = Math.random()*255
+    let green = Math.random()*255
+    let blue = Math.random()*255
+    const rgb = `rgb(${red},${green},${blue})`
+    let pad = +properties.get('--slant');
+    let slant = +properties.get('--slant');
+
+    ctx.moveTo(0, 0);
+    ctx.lineTo(pad + (size.width - slant - pad) * multiplier, 0);
+    ctx.lineTo(pad + (size.width - slant - pad) * multiplier + slant, size.height);
+    ctx.lineTo(0, size.height);
+    ctx.fillStyle = rgb;
+    ctx.fill();
+    // eval(properties.get('--my-background').toString())(ctx, size, properties);
   }
 })
